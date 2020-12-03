1ecf301f999680ea26c37a7e4c2676a7540b8d1c
Cucorova Veronika <veronika.cucorova@aalto.fi>
2020-12-03 18:02:20 +0200

    Added another convolutional layer

--- agents/DQN/dqn_agent.py
+++ agents/DQN/dqn_agent.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 import random
 
 Transition = namedtuple('Transition',
@@ -15,26 +16,35 @@ class PongDQN(nn.Module):
         super(PongDQN, self).__init__()
         self.linear_input_dim = self.linear_input(state_space_dim)
 
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.inputs = state_space_dim[0] * state_space_dim[1]
 
-        self.conv1 = nn.Conv2d(3, hidden, kernel_size=5, stride=2)
-        self.conv2 = nn.Conv2d(hidden, 32, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=1)
         self.fc1 = torch.nn.Linear(self.linear_input_dim, hidden)
         self.fc2 = torch.nn.Linear(hidden, action_space_dim)
+
+        my_layers = [self.conv1, self.conv2, self.conv3, self.fc1, self.fc2]
+        for l in my_layers:
+            l.to(self.device)
+        print("NN device", self.device)
         self.initialize()
 
     def initialize(self):
-        torch.nn.init.xavier_uniform_(self.conv1.weight)
-        torch.nn.init.xavier_uniform_(self.conv2.weight)
-        torch.nn.init.xavier_uniform_(self.fc1.weight)
-        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        torch.nn.init.kaiming_normal_(self.conv1.weight)
+        torch.nn.init.kaiming_normal_(self.conv2.weight)
+        torch.nn.init.kaiming_normal_(self.conv3.weight)
+        torch.nn.init.kaiming_normal_(self.fc1.weight)
+        torch.nn.init.kaiming_normal_(self.fc2.weight)
 
     def linear_input(self, state_space_dim):
-        a = self.conv2d_dims(50, 5, 2)
-        a = self.conv2d_dims(a, 5, 2)
-        b = self.conv2d_dims(50, 5, 2)
-        b = self.conv2d_dims(b, 5, 2)
+        a = self.conv2d_dims(50, 8, 4)
+        a = self.conv2d_dims(a, 4, 2)
+        a = self.conv2d_dims(a, 3, 1)
+        b = self.conv2d_dims(50, 8, 4)
+        b = self.conv2d_dims(b, 4, 2)
+        b = self.conv2d_dims(b, 3, 1)
         return a * b * 32
 
     def conv2d_dims(self, input, kernel_size, stride):
@@ -43,29 +53,31 @@ class PongDQN(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         x = x.view(-1, self.linear_input_dim)
-        x = self.fc1(x)
+        x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
 
 
 class DQNAgent(object):
     def __init__(self):
-        self.device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.name = "AVe"  # A-letta & Ve-ronika hehe
         self.n_actions = 3
         self.state_space = (50, 50)
-        self.batch_size = 256
+        self.batch_size = 128
         self.hidden = 16
         self.policy_net = PongDQN(self.state_space, self.n_actions, self.hidden, self.batch_size)
         self.target_net = PongDQN(self.state_space, self.n_actions, self.hidden, self.batch_size)
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
-        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=5e-4)
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=1e-4)
         self.memory = ReplayMemory(50000)
         self.gamma = 0.99
         self.prev_1 = np.zeros((50, 50))
         self.prev_2 = np.zeros((50, 50))
+        print("Agent device", self.device)
 
     def get_name(self):
         return self.name
@@ -75,8 +87,8 @@ class DQNAgent(object):
         self.prev_2 = np.zeros((50, 50))
 
     def update_network(self, updates=1):
-            for _ in range(updates):
-                self._do_network_update()
+        for _ in range(updates):
+            self._do_network_update()
 
     def _do_network_update(self):
         if len(self.memory) < self.batch_size:
@@ -102,8 +114,8 @@ class DQNAgent(object):
         expected_state_action_values = reward_batch + self.gamma * next_state_values
 
         # Compute Huber loss
-        loss = F.smooth_l1_loss(state_action_values.squeeze(),
-                                expected_state_action_values)
+        loss = F.smooth_l1_loss(state_action_values.squeeze(), expected_state_action_values)
+
         # Optimize the model
         self.optimizer.zero_grad()
         loss.backward()
@@ -117,7 +129,7 @@ class DQNAgent(object):
         if sample > epsilon:
             with torch.no_grad():
                 state = torch.reshape(state, (-1, 3, 50, 50))
-                #state = torch.from_numpy(state.reshape(-1, 3, 50, 50)).float()
+                # state = torch.from_numpy(state.reshape(-1, 3, 50, 50)).float()
                 q_values = self.policy_net(state)
                 return torch.argmax(q_values).item()
         else:
@@ -138,26 +150,10 @@ class DQNAgent(object):
         self.memory.push(stacked_state, action, stacked_next_state, reward, done)
 
     def preprocess(self, observation, prev_1=None, prev_2=None):
+
         observation = observation[::4, ::4, 0]  # downsample by factor of 4
         observation[observation == 33] = 0  # erase background
         observation[(observation == 75) | (observation == 202) | (observation == 255)] = 1
-
-        # show after color encoding
-        # plt.imshow(observation)
-        # plt.colorbar()
-        # plt.title("Observation after downsampling and color encoding")
-        # plt.show()
-        # memorize the previous state
-        # subtract_next = observation
-        # previous_observation = np.asarray(previous_observation)
-        # if previous_observation.any():
-        #    observation = observation - 0.5 * previous_observation
-        # observation = observation.astype(np.float)
-        # Show after substraction
-        # plt.imshow(observation)
-        # plt.colorbar()
-        # plt.title("Observation after subtraction of previous image")
-        # plt.show()
 
         if prev_1 is None:
             prev_1 = self.prev_1
@@ -165,9 +161,15 @@ class DQNAgent(object):
             prev_2 = self.prev_2
 
         stacked = np.concatenate((self.prev_1, self.prev_2, observation), axis=-1)
-        stacked = torch.from_numpy(stacked).float().unsqueeze(0)
+        stacked = torch.from_numpy(stacked).float().unsqueeze(0).to(self.device)
 
         return stacked, observation
+
+    def load_model(self):
+        weights = torch.load("model.mdl", map_location=torch.device(self.device))
+        self.policy_net.load_state_dict(weights, strict=False)
+        self.target_net.load_state_dict(weights, strict=False)
+        self.target_net.eval()
 
 
 class ReplayMemory(object):
